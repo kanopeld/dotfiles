ad9c2be11c9f4e7601d386a1f6fef3ef7aabe3ec
Kanopeld <i.beltsiukou@gmail.com>
2021-12-27 14:36:11 +0300

    git and keyboard fix

---
+++ git/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git config --global core.editor vim
+git config --global pull.rebase false
---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp varmilo-fix-fn.sh /usr/bin/fix_fn
+chmod +x /usr/bin/fix_fn
